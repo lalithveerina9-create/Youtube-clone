2bb669c83435be5327b33c59c3b11f8061621fe6
lalith <lalithveerina9@gmail.com>
2026-06-27 21:52:03 +0530

    Add deployment dependencies

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -19,15 +21,13 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/youtube-clone")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
   })
   .catch((err) => {
     console.log("MongoDB Error:", err);
   });
-
-console.log("Trying to connect...");
 
 // API Routes
 app.use("/auth", authRoutes);
@@ -40,8 +40,7 @@ app.use("/likedvideo", likedVideoRoutes);
 app.use("/watchlater", watchLaterRoutes);
 app.use("/subscription", subscriptionRoutes);
 
-// Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
