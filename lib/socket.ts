ab2c55a9bfb923aab1527309e1a28f76a1ce81de
lalithveerina9-create <lalithveerina9@gmail.com>
2026-08-19 18:15:37 +0530

    Configure frontend API and Socket URLs

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  autoConnect: false,
-});
+const socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL ||
+    "http://localhost:5000",
+  {
+    autoConnect: false,
+  }
+);
 
 export default socket;
